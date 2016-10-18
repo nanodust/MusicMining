ac31c1b0e2d3776728ee377d6fc8f5920477396c
AlexHorn <ahorn@shoppertrak.com>
2016-10-17 22:46:06 -0500

    auto-generated variable types w/ rattle, and PCA and AOE on numerics

--- R/csc424_MSD_0.r
+++ R/csc424_MSD_0.r
@@ -1,7 +1,14 @@
-install.packages("pairsD3")
 require("pairsD3")
-
+require("psych")
+require('ggplot2')
+library(corrgram)
 music <- read.csv("db_10k.csv",header=TRUE)
+describe(music)
+head(music)
+dim(music)
+library(rattle)
+
+ggplot(music)
 
 #ridding of NA values
 music$artist_terms_freq[is.na(music$artist_terms_freq)] <- 0
@@ -16,11 +23,9 @@ music$song_hotttnesss <- as.numeric(music$song_hotttnesss)
 
 vars <- colnames(music)
 
-head(music)
 
-dim(music)
 
-library(corrgram)
+
 corrgram(music, order=NULL, lower.panel=panel.ellipse, upper.panel=panel.pts,main="Corregram Matrix",pch=20) 
 
 # make correlation matrix
@@ -79,7 +84,94 @@ panel.cor <- function(x, y, digits=2, prefix="", cex.cor)
 
 
 
-
-
 pairs(music[,c(17,19,21,22,24)], lower.panel=panel.smooth,upper.panel=panel.cor,
       main="Million Song Database Scatterplot Matrix")
+
+
+
+# Build the training/validate/test datasets.
+
+crs$dataset <- read.csv("db_modified.csv", na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
+
+set.seed(993714) 
+crs$nobs <- nrow(crs$dataset) # 10000 observations 
+crs$sample <- crs$train <- sample(nrow(crs$dataset), 0.7*crs$nobs) # 7000 observations
+crs$validate <- sample(setdiff(seq_len(nrow(crs$dataset)), crs$train), 0.15*crs$nobs) # 1500 observations
+crs$test <- setdiff(setdiff(seq_len(nrow(crs$dataset)), crs$train), crs$validate) # 1500 observations
+
+# The following variable selections have been noted.
+
+crs$input <- c("artist_familiarity", "artist_hotttnesss", "artist_id", "artist_mbid",
+               "artist_playmeid", "artist_7digitalid", "artist_latitude", "artist_longitude",
+               "artist_location", "artist_name", "release", "release_7digitalid",
+               "song_hotttnesss", "title", "track_7digitalid", "similar_artists",
+               "artist_terms", "artist_terms_freq", "analysis_sample_rate", "audio_md5",
+               "danceability", "duration", "end_of_fade_in", "key",
+               "key_confidence", "loudness", "mode", "mode_confidence",
+               "start_of_fade_out", "tempo", "time_signature", "time_signature_confidence",
+               "segments_start", "segments_confidence", "segments_loudness_max", "segments_loudness_max_time",
+               "segments_loudness_start", "sections_start", "sections_confidence", "beats_start",
+               "beats_confidence", "bars_start", "bars_confidence", "tatums_start",
+               "tatums_confidence", "artist_mbtags", "artist_mbtags_count", "year")
+
+crs$numeric <- c("artist_familiarity", "artist_hotttnesss", "artist_playmeid", "artist_7digitalid",
+                 "artist_latitude", "artist_longitude", "artist_terms_freq", "danceability",
+                 "duration", "key", "key_confidence", "loudness",
+                 "mode", "mode_confidence", "start_of_fade_out", "tempo",
+                 "time_signature", "time_signature_confidence", "segments_start", "segments_confidence",
+                 "segments_loudness_max_time", "segments_loudness_start", "sections_start", "sections_confidence",
+                 "beats_start", "beats_confidence", "bars_start", "bars_confidence",
+                 "tatums_start", "tatums_confidence", "artist_mbtags_count", "year")
+
+crs$categoric <- c("artist_id", "artist_mbid", "artist_location", "artist_name",
+                   "release", "release_7digitalid", "song_hotttnesss", "title",
+                   "track_7digitalid", "similar_artists", "artist_terms", "analysis_sample_rate",
+                   "audio_md5", "end_of_fade_in", "segments_loudness_max", "artist_mbtags")
+
+crs$target  <- "artist_terms_weight"
+crs$risk    <- NULL
+crs$ident   <- c("song_id", "track_id")
+crs$ignore  <- c("energy", "X", "X.1", "X.2")
+crs$weights <- NULL
+
+library(Hmisc, quietly=TRUE)
+library(corrplot, quietly=TRUE)
+
+crs$cor <- cor(crs$dataset[crs$sample, crs$numeric], use="pairwise", method="pearson")
+
+# Order the correlations by their strength.
+
+crs$ord <- order(crs$cor[1,])
+crs$cor <- crs$cor[crs$ord, crs$ord]
+
+# Display the actual correlations.
+
+print(crs$cor)
+
+# Graphically display the correlations.
+
+corrplot(crs$cor, mar=c(0,0,1,0))
+title(main="Correlation db_modified.csv using Pearson")
+
+# Principal Components Analysis (on numerics only).
+
+pc <- princomp(na.omit(crs$dataset[crs$sample, crs$numeric]), scale=TRUE, center=TRUE, tol=0)
+
+# Show the output of the analysis.
+
+pc
+
+# Summarise the importance of the components found.
+
+summary(pc)
+
+# Display a plot showing the relative importance of the components.
+
+plot(pc, main="")
+title(main="Principal Components Importance db_modified.csv")
+
+
+# Display a plot showing the two most principal components.
+
+biplot(pc, main="")
+title(main="Principal Components db_modified.csv")
