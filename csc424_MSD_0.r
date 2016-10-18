7ef6b24a3878615f6eb40c00d5a36416140815fa
AlexHorn <ahorn@shoppertrak.com>
2016-10-17 21:45:06 -0500

    failing corgram & corrplot, successful Shiny

---
+++ csc424_MSD_0.r
@@ -0,0 +1,34 @@
+install.packages("pairsD3")
+require("pairsD3")
+
+music <- read.csv("db.csv",header=TRUE)
+
+#ridding of NA values
+music$artist_terms_freq[is.na(music$artist_terms_freq)] <- 0
+music$artist_familiarity[is.na(music$artist_familiarity)] <- 0
+#convert wrongly assigned types
+music$artist_terms_weight <- as.numeric(music$artist_terms_weight)
+music$analysis_sample_rate <- as.numeric(music$analysis_sample_rate)
+music$end_of_fade_in <- as.numeric(music$end_of_fade_in)
+music$artist_hotttnesss<- as.numeric(music$artist_hotttnesss)
+music$segments_loudness_max<- as.numeric(music$segments_loudness_max)
+music$song_hotttnesss <- as.numeric(music$song_hotttnesss)
+
+vars <- colnames(music)
+
+head(music)
+
+dim(music)
+
+library(corrgram)
+corrgram(music, order=NULL, lower.panel=panel.ellipse, upper.panel=panel.pts,main="Corregram Matrix",pch=20) 
+
+# make correlation matrix
+music_cor = cor(music)
+
+# visualize it
+corrplot(music_cor, method="ellipse",order="AOE")
+
+# try shiny
+
+shinypairs(music)
