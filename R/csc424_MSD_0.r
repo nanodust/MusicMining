407c378e11ae845e096156b5c3dbcdf697c1a4cd
AlexHorn <ahorn@shoppertrak.com>
2016-10-19 20:05:33 -0500

    fixed error w/ data & R, added graph output..

--- R/csc424_MSD_0.r
+++ R/csc424_MSD_0.r
@@ -91,7 +91,9 @@ pairs(music[,c(17,19,21,22,24)], lower.panel=panel.smooth,upper.panel=panel.cor,
 
 # Build the training/validate/test datasets.
 
-crs$dataset <- read.csv("db_10k.csv", na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
+crs <- new.env(parent = baseenv())
+
+crs$dataset <- read.csv("db_modified.csv", na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
 
 set.seed(993714) 
 crs$nobs <- nrow(crs$dataset) # 10000 observations 
